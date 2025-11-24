b8c9175b8de4c62965cfe795c40aa98118fadd24
mr-n30 <root@localhost>
2025-11-24 00:11:12 -0800

    Test run

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,7 +7,24 @@ def emotion_detector(text_to_analyse: str) -> dict:
         headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
         r = requests.post(url, headers=headers, json=body).json()
-        return r
+
+        anger = r['anger_score']
+        disgust = r['disgust_score']
+        fear = r['fear_score']
+        joy = r['joy_score']
+        sadness = r['sadness_score']
+        dominant_emotion = max([anger, disgust, fear, joy, sadness])
+
+        response_dict = {
+            'anger': anger,
+            'disgust': disgust,
+            'fear': fear,
+            'joy': joy,
+            'sadness': sadness,
+            'dominant_emotion': dominant_emotion
+        }
+
+        return response_dict
     except Exception as e:
         print(f'Error occurred=>{e}')
         return {}
