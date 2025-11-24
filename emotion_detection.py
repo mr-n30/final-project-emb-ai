7e3ab10fe32e223e6a249c355d995c653d9bb510
mr-n30 <root@localhost>
2025-11-24 00:03:27 -0800

    Fixed from get to post

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,7 +6,7 @@ def emotion_detector(text_to_analyse: str) -> dict:
         url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
         headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
-        r = requests.get(url, headers=headers, json=body).json()
+        r = requests.post(url, headers=headers, json=body).json()
         return r
     except Exception as e:
         print(f'Error occurred=>{e}')
